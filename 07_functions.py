42bce3908366d248e694c9b17e00e5e724c5ecb5
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-20 16:35:40 -0500

    Manejo de funciones

---
+++ 07_functions.py
@@ -0,0 +1,17 @@
+
+print('Hola mundo !!!')
+# para definir una funciona usamos la palabra reservada def
+def my_print(text):
+    print(text * 2)
+
+my_print('Soy Raul ')
+my_print('Hola ')
+
+a = 10
+b = 90
+c = a + b
+
+def suma(a, b):
+    return a + b
+
+my_print(suma(a, b))
