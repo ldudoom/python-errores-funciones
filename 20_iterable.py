9ed9dc77d14ed66222fbf5f004017c3f743c0d1c
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-26 11:19:30 -0500

    Manejo manual del objeto iterator

---
+++ 20_iterable.py
@@ -0,0 +1,10 @@
+for i in range(1, 11):
+    print(i)
+
+my_iterable = iter(range(1, 4))
+print(my_iterable)
+print(next(my_iterable))
+print(next(my_iterable))
+print(next(my_iterable))
+print(next(my_iterable))
+print(next(my_iterable))
