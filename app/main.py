37540822035d92c5b707ccc6972e7c757634d234
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-27 08:10:33 -0500

    Generando un grafico desde datos de un CSV

--- app/main.py
+++ app/main.py
@@ -1,38 +1,19 @@
 import utils
-
-data = [
-        {
-            'Country': 'Ecuador',
-            'Population': 500
-        },
-        {
-            'Country': 'Colombia',
-            'Population': 600
-        },
-        {
-            'Country': 'Argentina',
-            'Population': 600
-        },
-        {
-            'Country': 'Brasil',
-            'Population': 1200
-        },
-        {
-            'Country': 'Mexico',
-            'Population': 800
-        },
-        {
-            'Country': 'Canada',
-            'Population': 1200
-        }
-    ]
+import read_csv
+import charts
 
 def run():
-    keys, values = utils.get_population()
-    print(keys, values)
-
+    
+    data = read_csv.read_csv('./app/data.csv')
     country = input('Ingrese el pais a buscar: ')
     result = utils.population_by_country(data, country.title())
+
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
+        
+
     print(result)
 
 
--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,20 @@
-def get_population():
-    keys = ['Ecuador', 'Colombia', 'Canada', 'EE.UU.', 'Mexico', 'Bolivia', 'Argentina', 'Brasil', 'Chile']
-    values = [500, 600, 1200, 1500, 900, 300, 650, 1300, 700]
-    return keys, values
+def get_population(country_dict):
+    
+    population_dict = {
+        '2022': int(country_dict['2022 Population']),
+        '2020': int(country_dict['2020 Population']),
+        '2015': int(country_dict['2015 Population']),
+        '2010': int(country_dict['2010 Population']),
+        '2000': int(country_dict['2000 Population']),
+        '1990': int(country_dict['1990 Population']),
+        '1980': int(country_dict['1980 Population']),
+        '1970': int(country_dict['1970 Population'])
+    }
+    
+    labels = population_dict.keys()
+    values = population_dict.values()
+
+    return labels, values
 
 def population_by_country(data, country):
-    return list(filter(lambda item: item['Country'] == country, data))
+    return list(filter(lambda item: item['Country/Territory'] == country, data))
